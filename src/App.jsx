4ce58c51d19632604ee1433f3cc443fecade4f36
Eray-Adiyaman <erayadiyaman@outlook.com>
2023-03-08 14:24:48 +0300

    Routes,LoaderFunctions,Few Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,31 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+import Dashboard, { dashboardLoader } from "./pages/Dashboard";
+import Error from "./pages/Error";
+
+
+// Routes \\
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+    loader: dashboardLoader,
+    errorElement: <Error />
+  },
+  {
+    path:"*",
+    element: <Error />  
+  }
+]);
+
+
 function App() {
   return <div className="App">
-    init
+    <RouterProvider router={router} />
   </div>;
 }
 
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,24 @@
+//ReactRouter imports
+import { useLoaderData } from "react-router-dom";
+
+//helper functions
+import { fetchData } from "../helpers"
+
+//Loader Funtion
+export function dashboardLoader(){
+    const userName = fetchData("userName");
+    return { userName }
+}
+
+
+export default function Dashboard() {
+
+   const {userName} =  useLoaderData()
+
+  return (
+    <div>
+        <h1>{userName}</h1>
+        Dashboard
+    </div>
+  )
+}

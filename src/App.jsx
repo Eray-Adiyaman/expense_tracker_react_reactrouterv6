d38defdcc136a41a51cc3055f16dfc6b337ad05f
Eray-Adiyaman <erayadiyaman@outlook.com>
2023-03-09 19:01:47 +0300

    user form submit

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 
 import Main, { mainLoader } from "./layouts/Main";
-import Dashboard, { dashboardLoader } from "./pages/Dashboard";
+import Dashboard, { dashboardAction, dashboardLoader } from "./pages/Dashboard";
 import Error from "./pages/Error";
 
 // Routes \\
@@ -21,6 +21,7 @@ const router = createBrowserRouter([
         index: true,
         element: <Dashboard />,
         loader: dashboardLoader,
+        action: dashboardAction,
         errorElement: <Error />,
       },
       {
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,8 @@
 //ReactRouter imports
 import { useLoaderData } from "react-router-dom";
+
 import Intro from "../components/Intro";
+import { toast } from "react-toastify";
 
 //helper functions
 import { fetchData } from "../helpers"
@@ -9,6 +11,22 @@ import { fetchData } from "../helpers"
 export function dashboardLoader(){
     const userName = fetchData("userName");
     return { userName }
+}
+
+//action
+//since intro component has the form and the forms submited to the page they live on , 
+//so its submited to here dashboard and will handle the submit action here,
+//since its a request the request obj can we destructured here
+export async function dashboardAction({ request }){
+ const data = await request.formData();
+ const formData = Object.fromEntries(data);
+ try {
+   localStorage.setItem("userName",JSON.stringify(formData.userName))
+   return toast.success(`Welcome, ${formData.userName}`)
+ } catch (error) {
+  throw new Error("There was an error while creating your account.")
+ }
+ console.log({data,request},formData)
 }
 
 
